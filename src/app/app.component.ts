385d71e5ddb9e8cb31262be2ab960d589e5962e6
Alex.Ondo <alex.ondo@lmdsio.fr>
2024-11-07 16:36:39 +0100

    v1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {PraticienComponent} from './praticien/praticien.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PraticienComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'gestionGSB';
 }
---
+++ src/app/praticien/praticien.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-praticien',
+  standalone: true,
+  imports: [],
+  templateUrl: './praticien.component.html',
+  styleUrl: './praticien.component.scss'
+})
+export class PraticienComponent {
+
+}
