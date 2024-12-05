37b5b6a7dc7979e5e9342ce28d04d044c6647cf2
Alex.Ondo <alex.ondo@lmdsio.fr>
2024-12-05 17:18:31 +0100

    v1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
 import {PraticienComponent} from './praticien/praticien.component';
 import{Praticien} from './models/praticien';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PraticienComponent],
+  imports: [PraticienComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+
 })
 
 export class AppComponent implements OnInit {
-  praticiens!: Praticien[];
-
-
   ngOnInit() {
-    this.praticiens = [
-      new Praticien('https://www.w3schools.com/howto/img_avatar2.png', 'Smith', 'Anna', '10 rue de la Santé', '75001', 'Paris', 'À visiter'),
-      new Praticien('https://www.w3schools.com/howto/img_avatar.png', 'Brown', 'James', '20 rue de la Médecine', '75002', 'Paris', 'À visiter'),
-      new Praticien('https://www.w3schools.com/howto/img_avatar3.png', 'Johnson', 'Emily', '30 rue des Hôpitaux', '75003', 'Paris', 'Déjà visité'),
-    ];
   }
+
 }
--- src/app/praticien/praticien.component.ts
+++ src/app/praticien/praticien.component.ts
@@ -1,31 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import{Praticien} from '../models/praticien';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 @Component({
   selector: 'app-praticien',
   standalone: true,
-  imports: [ ],
+  imports: [TitleCasePipe,UpperCasePipe,LowerCasePipe],
   templateUrl: './praticien.component.html',
   styleUrls: ['./praticien.component.scss']
 })
-export class PraticienComponent implements OnInit {
-  imageUrl!: string;
-  nom!: string;
-  prenom!: string;
-  adresse!: string;
-  cp!: string;
-  ville!: string;
-  private statut!: string;
-
-  ngOnInit() {
-    this.imageUrl = 'https://www.w3schools.com/howto/img_avatar.png';
-    this.nom = 'Doe';
-    this.prenom = 'John';
-    this.adresse = '1 rue des malades';
-    this.cp = '75888';
-    this.ville = 'Paris';
-  }
+export class PraticienComponent  {
+  @Input() praticien!: Praticien;
 
   onModificationStatut(): void {
-    this.statut = this.statut === 'À visiter' ? 'Déjà visité' : 'À visiter';
+    this.praticien.statut = this.praticien.statut === 'À visiter' ? 'Déjà visité' : 'À visiter';
   }
 }
